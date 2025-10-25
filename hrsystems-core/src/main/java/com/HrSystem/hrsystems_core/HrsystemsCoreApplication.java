eabb54f176e87b0335857fe4af19e0f50cb88800
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-10-25 14:22:44 +0530

    Initialize hrsystems-core Spring Boot module
    Add initial Gradle project structure for the hrsystems-core module, including build scripts, wrapper, .gitignore, and .gitattributes. Implement a basic Spring Boot application entry point and a test class. Set up application properties and project settings.

---
+++ hrsystems-core/src/main/java/com/HrSystem/hrsystems_core/HrsystemsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.HrSystem.hrsystems_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsystemsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsystemsCoreApplication.class, args);
+	}
+
+}
