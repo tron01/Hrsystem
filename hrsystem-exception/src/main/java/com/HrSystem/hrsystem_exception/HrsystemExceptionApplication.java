8bb57e55e2958d650c7b03f4091cea4a0d398994
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-10-25 14:25:06 +0530

    Initialize hrsystem-exception Spring Boot module
    Add a new Gradle-based Spring Boot project named 'hrsystem-exception' with basic configuration, application entry point, and a context load test. Includes Gradle wrapper, build scripts, and initial application properties.

---
+++ hrsystem-exception/src/main/java/com/HrSystem/hrsystem_exception/HrsystemExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.HrSystem.hrsystem_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsystemExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsystemExceptionApplication.class, args);
+	}
+
+}
